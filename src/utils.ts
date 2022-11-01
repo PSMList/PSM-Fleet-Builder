1c8e046f4e99c3e0af389c873b0ba23acd48db6c
Dorian Souleyreau <doriansouleyreau@gmail.com>
2022-11-01 17:26:49 +0100

    Added a string capitalize function

--- src/utils.ts
+++ src/utils.ts
@@ -10,3 +10,7 @@ export function removeItemFromArray<T>(array: Array<T>, func: (value: T, index: 
 export function isStrPositiveInt(str: string) {
     return /^\+?(0|[1-9]\d*)$/.test(str);
 }
+
+export function capitalize(value: string) {
+    return value.charAt(0).toLocaleUpperCase() + value.slice(1)
+}
