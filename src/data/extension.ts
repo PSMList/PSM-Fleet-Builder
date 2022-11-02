12fcbbabab27e263cced758bc8b030dd597a374b
Dorian Souleyreau <doriansouleyreau@gmail.com>
2022-11-02 19:35:32 +0100

    Fixed RSS extension name

--- src/data/extension.ts
+++ src/data/extension.ts
@@ -12,7 +12,7 @@ type ExtensionDataItem = [
 ]
 
 const extensionData: ExtensionDataItem[] = JSON.parse(
-    `[[2,"Pirates of the Spanish Main","SM"],[3,"Pirates of the Crimson Coast","CC"],[4,"Pirates of the Revolution","RV"],[5,"Pirates of the Barbary Coast","BC"],[6,"Pirates of the South China Seas","SCS"],[7,"Pirates of the Davy Jones' Curse","DJC"],[8,"Pirates of the Mysterious Islands","MI"],[9,"Pirates of the Frozen North","FN"],[10,"Pirates at Ocean's Edge","OE"],[11,"Pirates of Carribean","CA"],[12,"Pirates of the Cursed Seas : Rise of Fiends","RF"],[13,"Pirates of the Cursed Seas : Fire and Steel","FS"],[14,"Pirates of the Savages Shores","SS"],[19,"Pirates of the Spanish Main Unlimited","SMU"],[20,"Pirates of the Revolution Unlimited","RVU"],[21,"Pirates of the Barbary Coast Unlimited","BCU"],[22,"Return to Savage Shores","RSS'"]]`
+    `[[2,"Pirates of the Spanish Main","SM"],[3,"Pirates of the Crimson Coast","CC"],[4,"Pirates of the Revolution","RV"],[5,"Pirates of the Barbary Coast","BC"],[6,"Pirates of the South China Seas","SCS"],[7,"Pirates of the Davy Jones' Curse","DJC"],[8,"Pirates of the Mysterious Islands","MI"],[9,"Pirates of the Frozen North","FN"],[10,"Pirates at Ocean's Edge","OE"],[11,"Pirates of Carribean","CA"],[12,"Pirates of the Cursed Seas : Rise of Fiends","RF"],[13,"Pirates of the Cursed Seas : Fire and Steel","FS"],[14,"Pirates of the Savages Shores","SS"],[19,"Pirates of the Spanish Main Unlimited","SMU"],[20,"Pirates of the Revolution Unlimited","RVU"],[21,"Pirates of the Barbary Coast Unlimited","BCU"],[22,"Return to Savage Shores","RSS"]]`
 );
 
 const extensions: { [id: string]: ExtensionType } = {};
