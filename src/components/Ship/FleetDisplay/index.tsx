6a7902ac3ff2a150ddaea092509f28da24168420
Dorian Souleyreau <doriansouleyreau@gmail.com>
2022-11-03 02:31:50 +0100

    Fixed layout of Settings in modal

--- src/components/Ship/FleetDisplay/index.tsx
+++ src/components/Ship/FleetDisplay/index.tsx
@@ -201,7 +201,6 @@ const FleetDisplay = () => {
             },
             inside:
                 <Settings
-                    class="main_container"
                     key={ Math.random() } // will surely reset the component (reset default data)
                     defaultData={{
                         name: defaultFleetData.name,
