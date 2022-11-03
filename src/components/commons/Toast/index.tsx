316b869966a4d71c6025d902cc419ef79fbbd204
Dorian Souleyreau <doriansouleyreau@gmail.com>
2022-11-03 18:10:39 +0100

    Changed Toast image from SVG assets to FontAwesome icons

--- src/components/commons/Toast/index.tsx
+++ src/components/commons/Toast/index.tsx
@@ -1,18 +1,14 @@
 import { capitalize } from "../../../utils";
-import InfoIcon from '../../../assets/ToastIcons/info.svg';
-import SuccessIcon from '../../../assets/ToastIcons/success.svg';
-import WarningIcon from '../../../assets/ToastIcons/warning.svg';
-import ErrorIcon from '../../../assets/ToastIcons/error.svg';
 import IconButton from "../IconButton";
 
 
 type ToastTypes = 'info' | 'success' | 'warning' | 'error';
 
 const icons: { [K in ToastTypes]: string } = {
-    info: InfoIcon,
-    success: SuccessIcon,
-    warning: WarningIcon,
-    error: ErrorIcon
+    info: 'info-circle',
+    success: 'check-circle',
+    warning: 'exclamation-triangle',
+    error: 'exclamation-circle'
 }
 
 export type ToastPosition = 'top-right' | 'bottom-right' | 'top-left' | 'bottom-left';
@@ -53,7 +49,7 @@ const Toast = ({ id, type, position, title, description, deleteToast }: ToastPro
             style={{ backgroundColor: backgroundColor }}
         >
             <IconButton iconID="times" onClick={() => deleteToast(id)} />
-            <img src={ icons[type] } alt={ title } title={ title } className="notification-image" />
+            <i class={ "notification-image fas fa-" + icons[type] } alt={ title } title={ title } />
             <p className="notification-title">{title}</p>
             <p className="notification-message">
                 {description}
