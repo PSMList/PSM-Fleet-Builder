93af0befdf771858ef912d861c106a59a3f1fa1c
Dorian Souleyreau <doriansouleyreau@gmail.com>
2023-09-11 23:01:04 +0200

    Improve button style + add following text

--- src/components/commons/IconButton/index.tsx
+++ src/components/commons/IconButton/index.tsx
@@ -1,21 +1,33 @@
-import { JSX, splitProps } from "solid-js";
+import { JSX, splitProps, Show } from "solid-js";
 import Icon from "../Icon";
 import "./IconButton.css";
 
 type IconButtonProps = {
   iconID: string;
+  showTitle?: boolean;
 } & JSX.IntrinsicAttributes &
   JSX.HTMLAttributes<HTMLButtonElement>;
 
 const IconButton = (props: IconButtonProps) => {
-  const [localProps, buttonProps] = splitProps(props, ["iconID", "classList", "class"]);
+  const [localProps, buttonProps] = splitProps(props, [
+    "iconID",
+    "classList",
+    "class",
+  ]);
 
   return (
     <button
       {...buttonProps}
-      classList={{ "icon_button": true, [localProps.class || ""]: true, ...localProps.classList }}
+      classList={{
+        icon_button: true,
+        [localProps.class ?? ""]: true,
+        ...localProps.classList,
+      }}
     >
       <Icon iconID={localProps.iconID} />
+      <Show when={props.showTitle}>
+        <span class="title">{props.title}</span>
+      </Show>
     </button>
   );
 };
